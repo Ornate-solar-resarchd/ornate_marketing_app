614dd355eaf3816e9a52109ab0919fc2a04c7589
cool2568 <darpan1871@gmail.com>
2026-03-17 11:30:59 +0530

    deployment

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "ornatesolar.com" },
       { protocol: "https", hostname: "www.solaredge.com" },
       { protocol: "https", hostname: "*.amazonaws.com" },
+      { protocol: "https", hostname: "i.ibb.co" },
     ],
   },
   transpilePackages: ["@ornate/types"],
